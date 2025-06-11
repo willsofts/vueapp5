eb9bae15bc0e79f58d6654242260c7a211184f53
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-11 13:49:17 +0700

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,6 +1,8 @@
 const appVariables = {
+    API_URL: "http://localhost:8080",
+    BASE_URL: "http://localhost:8080",
+	IMG_URL: "http://localhost:8080",
     MULTI_LANGUAGES: ["EN","TH"],
-    BASE_CSS: "./css/theme_green_sea.css"
 };
 function getAppConfigs() {
     return appVariables;
